a1f560633f95e93409864379e837171dcb7c761a
Kevin Nguyen <kvn219@nyu.edu>
2017-06-24 21:25:02 -0400

    update

--- {{cookiecutter.repo_name}}/app/app.py
+++ {{cookiecutter.repo_name}}/app/app.py
@@ -1,1 +1,9 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
+# Instantiate the app
+app = Flask(__name__)
+
+# Run the application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
